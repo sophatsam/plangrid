54c36e9bd69771a04ace4910d78b263fad9570b4
Sophat Sam <sophat.sam@gmail.com>
2017-07-10 12:59:10 -0400

    update Sequelize migration to include new project fields

--- migrations/20161003154622-project-migration.js
+++ migrations/20161003154622-project-migration.js
@@ -7,6 +7,18 @@ module.exports = {
       uid: Sequelize.STRING,
       project_name: Sequelize.STRING,
       project_id: Sequelize.STRING,
+      custom_id: Sequelize.STRING,
+      type: Sequelize.STRING,
+      status: Sequelize.STRING,
+      owner: Sequelize.STRING,
+      start_date: Sequelize.DATE,
+      end_date: Sequelize.DATE,
+      street_1: Sequelize.STRING,
+      street_2: Sequelize.STRING,
+      city: Sequelize.STRING,
+      region: Sequelize.STRING,
+      postal_code: Sequelize.STRING,
+      country: Sequelize.STRING,
       created_at: Sequelize.DATE, // date PG project was created
       updated_at: Sequelize.DATE, // date PG project was updated
       updatedAt: Sequelize.DATE,  // date this record was updated
